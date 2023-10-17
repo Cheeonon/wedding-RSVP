bcebb720b52e756764bf7f64efd100b47c6a12af
cheeonon <cheeon00@gmail.com>
2023-10-17 05:45:15 -0400

    Fixes port number

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 
 const DATA_PATH = path.join(__dirname, './data/guests.json');
 
