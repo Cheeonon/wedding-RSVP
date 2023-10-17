503342be3d8bde27ac4068cf0fa3b39429dc1334
cheeonon <cheeon00@gmail.com>
2023-10-17 04:14:55 -0400

    Develops submit function

--- server/server.js
+++ server/server.js
@@ -2,27 +2,54 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 const PORT = 5000;
+
+const DATA_PATH = path.join(__dirname, './data/guests.json');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello, Worldd!');
+    res.send('Hello, World!');
 });
+
+
+const readData = () => {
+    try {
+        const rawData = fs.readFileSync(DATA_PATH, 'utf-8');
+
+        return JSON.parse(rawData);
+    } catch (error) {
+        throw new Error('Failed to read data file.');
+    }
+
+};
+
+const writeData = (data) => {
+    try {
+        fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2));
+    } catch (error) {
+        throw new Error('Failed to write to data file.');
+    }
+};
 
 app.post('/api/submit', (req, res) => {
-    const formData = req.body;
-
-    // You can process the formData, save it to a database, etc.
-
-    console.log(formData);
-
-    // Respond to the client
-    res.json({ message: 'Form data received successfully!' });
+    const newData = req.body;
+    
+    try {
+        const dataFile = readData();
+        dataFile.push(newData);
+        writeData(dataFile);
+        
+        res.json({ message: 'RSVP submitted successfully.', data: dataFile });
+    } catch (err) {
+        console.error('Error processing RSVP:', err);
+        res.status(500).json({ message: 'Server error.' });
+    }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
